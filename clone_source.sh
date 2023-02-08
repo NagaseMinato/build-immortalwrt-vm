a208808bc9d9113ecfcfa26478f70120c8edfdda
AkinoKaede <autmaple@protonmail.com>
2023-02-08 18:08:53 +0800

    add two patches

--- clone_source.sh
+++ clone_source.sh
@@ -2,3 +2,6 @@
 
 git clone "https://github.com/immortalwrt/immortalwrt.git" --branch "openwrt-21.02" --single-branch "immortalwrt"
 pushd "immortalwrt"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
+
+patch immortalwrt/package/base-files/files/bin/config_generate config_generate.patch
+patch immortalwrt/package/base-files/files/etc/shadow shadow.patch
